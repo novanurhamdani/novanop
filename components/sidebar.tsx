c062631eabe1ebc8b0f0c30cf19e4c6b66eb2bc1
novanurhamdani <nova.nurhamdani@gmail.com>
2024-11-02 12:59:14 +0700

    feat: improve theme toggle and add logo novanop

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,24 +1,32 @@
-import { Code2, User2 } from "lucide-react";
+import { User2 } from "lucide-react";
 import { ThemeToggle } from "./theme-toogle";
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export function Sidebar() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <aside className="fixed left-0 top-0 z-40 h-screen w-64 border-r bg-background">
       <div className="flex h-full flex-col px-4 py-8">
         <div className="mb-8 flex items-center gap-2">
-          <Code2 className="h-8 w-8" />
-          <h1 className="text-xl font-bold">Your Name</h1>
+          <Image
+            src={
+              theme === "dark" ? "/logo-novanop-white.png" : "/logo-novanop.png"
+            }
+            alt=""
+            width={50}
+            height={50}
+          />
+          <h1 className="text-xl font-bold">Nova Nurhamdani</h1>
+          <ThemeToggle theme={theme} setTheme={setTheme} />
         </div>
 
         <div className="mb-8">
           <div className="flex items-center gap-2">
             <User2 className="h-5 w-5" />
-            <span className="text-muted-foreground">Full Stack Developer</span>
+            <span className="text-muted-foreground">Software Engineer</span>
           </div>
-        </div>
-
-        <div className="mt-auto">
-          <ThemeToggle />
         </div>
       </div>
     </aside>
