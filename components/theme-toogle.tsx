92d7e932aa01b3881f87468673b868ba727f34d7
novanurhamdani <nova.nurhamdani@gmail.com>
2024-11-02 12:59:14 +0700

    feat: improve theme toggle and add logo novanop

--- components/theme-toogle.tsx
+++ components/theme-toogle.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
 
-export function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+type ThemeToggleProps = {
+  theme: string | undefined;
+  setTheme: (theme: string) => void;
+};
 
+export function ThemeToggle({ theme, setTheme }: ThemeToggleProps) {
   return (
-    // <button
-    //   onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-    //   className="rounded-md p-2 hover:bg-accent"
-    //   aria-label="Toggle theme"
-    // >
-    //   <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    //   <Moon className="h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    // </button>
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="flex items-center justify-center w-[50px] py-2 text-sm text-gray-700 dark:text-gray-300 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors"
@@ -24,7 +18,6 @@ export function ThemeToggle() {
       ) : (
         <Moon className="w-4 h-4" />
       )}
-      {/* {theme === "dark" ? "Light Mode" : "Dark Mode"} */}
     </button>
   );
 }
