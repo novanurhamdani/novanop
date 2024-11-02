a6350f9342c8bac51f5a35f7e0f7332a8caef177
novanurhamdani <nova.nurhamdani@gmail.com>
2024-11-02 13:53:23 +0700

    feat: add project and cv menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { CVContent } from "@/components/cv-content";
 import { ProjectCard } from "@/components/project-card";
 import { Sidebar } from "@/components/sidebar";
 import { useMaintenanceMode } from "@/hooks/use-maintenance-mode";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const MaintenanceMode = dynamic(() => import("@/components/maintenance"), {
   ssr: false,
@@ -29,6 +31,7 @@ const projects = [
 
 export default function Home() {
   const { isMaintenance, isClient } = useMaintenanceMode();
+  const [activeSection, setActiveSection] = useState("projects");
 
   if (!isClient) {
     return null;
@@ -40,13 +43,20 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen bg-background">
-      <Sidebar />
+      <Sidebar
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+      />
       <main className="ml-64 flex-1 p-8">
-        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project, index) => (
-            <ProjectCard key={index} {...project} />
-          ))}
-        </div>
+        {activeSection === "projects" ? (
+          <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {projects.map((project, index) => (
+              <ProjectCard key={index} {...project} />
+            ))}
+          </div>
+        ) : (
+          <CVContent />
+        )}
       </main>
     </div>
   );
